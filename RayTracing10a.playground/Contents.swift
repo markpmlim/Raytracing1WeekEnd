4ff00870f5a9aeeec79829e98892bbd9215d5da8
markpmlim <markpmlim@users.noreply.github.com>
2021-03-22 10:57:02 +0800

    Update Contents.swift

--- RayTracing10a.playground/Contents.swift
+++ RayTracing10a.playground/Contents.swift
@@ -1,5 +1,6 @@
 // 10. Dielectrics
 // Image 14: Glass sphere that always refracts
+// The playground will crash if the built-in refract function is called.
 import Cocoa
 
 let cgImage = generateCGImage()
